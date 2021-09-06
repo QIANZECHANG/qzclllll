3bbe111edbd150617a1607e09e47b850e0a9152f
QIANZECHANG <62879476+QIANZECHANG@users.noreply.github.com>
2021-09-06 16:35:06 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,25 @@
+struct M{int* p1;int* p2;};
+func(int a,int b){
+  struct M m;
+  m.p1=malloc(4);
+  m.p2=malloc(4); 
+  int c=a+b; 
+  int* n;
+  if(c>5)
+    n=m.p1;
+  else
+    n=malloc(4);
+  free(m.p1+8);
+  free(n);
+  if(c<=5)free(m.p1);
+}
+
+
+
+
+
+
+
 p = malloc(1);
 if (c > 1)
   q = p;
@@ -8,9 +30,43 @@ c = 2;
 free(q);
 
 
+#include <stdlib.h> 
+struct MemHider { void * hiddenData; };
+int main( int argc, char ** argv ) { 
+  struct MemHider someMemory; 
+  struct MemHider someMemory2; 
+  struct MemHider someMemory3; 
+  struct MemHider *alias = &someMemory; 
+  struct MemHider *alias3 = &someMemory3; 
+  someMemory.hiddenData = malloc(sizeof(char)); 
+  free(alias->hiddenData);
+  someMemory2.hiddenData = malloc(sizeof(char)); 
+  free((&someMemory2)->hiddenData); 
+  alias3->hiddenData = malloc(sizeof(char)); 
+  //free(someMemory3.hiddenData); 
+  return 0;
+}
 
-
-
+#include <stdlib.h>
+struct MemHider { void * hiddenData; }; 
+void dataHider1( void * ); 
+void dataHider2( void * ); 
+int main( int argc, char ** argv ) { 
+  struct MemHider someMemory;
+  struct MemHider someMemory2; 
+  dataHider1( &someMemory ); 
+  dataHider2( &someMemory2 ); 
+  free( someMemory.hiddenData ); 
+  free( someMemory2.hiddenData ); 
+  return 0; 
+} 
+void dataHider1( void * _this ) { 
+  struct MemHider * this = ( struct MemHider * )_this;
+  this->hiddenData = malloc( sizeof( char ) ); 
+} 
+void dataHider2( void * _this ) { 
+  ( ( struct MemHider * )_this)->hiddenData = malloc( sizeof( char ) ); 
+}
 
 
 
